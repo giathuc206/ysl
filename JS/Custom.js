aeeff4d97028ef509a3579ed6062d0dcc6ec8357
giathuc206 <157475814+giathuc206@users.noreply.github.com>
2024-01-23 16:29:48 +0700

    Add files via upload

---
+++ JS/Custom.js
@@ -0,0 +1,34 @@
+// JavaScript Document
+$('#banner').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('#bai-viet').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
